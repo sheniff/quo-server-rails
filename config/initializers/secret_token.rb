78070b6d23a10d46a48232e8bebcd3f30027ef0b
Sergio Almecija <sergio.almecija@tapjoy.com>
2013-03-13 13:37:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuoridorServerRails::Application.config.secret_token = '626d51c098ccb2ac9ba43877ac936641d0074d170910026079c7465bdc88dcf2e7f7023909a018df29d9aaae04143958be5c7fa0a53bb64c2b561345b2a82bb0'
